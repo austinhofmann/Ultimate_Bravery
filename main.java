d5d415538fd588120383ca1fbb961c50ad9753ea
austinhofmann <a916h636@ku.edu>
2014-04-05 19:36:56 -0500

    Update main.java

--- main.java
+++ main.java
@@ -4,12 +4,10 @@ import java.util.Random;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		items.main(args);
 		
-		
-		//items poop = new items("poop", "hello");
-		
+		//random test
 		System.out.println(items.getMenu(items.itemsArray[items.getBoots()]));
 		
 	}
